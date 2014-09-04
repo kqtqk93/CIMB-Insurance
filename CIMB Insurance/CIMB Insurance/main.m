182a08da784601a00a9967b4057133c266513667
shanmarkus <shanmarkus@gmail.com>
2014-09-04 11:45:44 +0700

    First Commit

---
+++ CIMB Insurance/CIMB Insurance/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CIMB Insurance
+//
+//  Created by Shan Markus on 9/4/14.
+//  Copyright (c) 2014 Vayas World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+    }
+}
---
+++ CIMB Insurance/CIMB InsuranceTests/CIMB_InsuranceTests.m
@@ -0,0 +1,34 @@
+//
+//  CIMB_InsuranceTests.m
+//  CIMB InsuranceTests
+//
+//  Created by Shan Markus on 9/4/14.
+//  Copyright (c) 2014 Vayas World. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CIMB_InsuranceTests : XCTestCase
+
+@end
+
+@implementation CIMB_InsuranceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
