79be5ebd8e4e945a20c2f795fd1aa7f66d497099
shanmarkus <shanmarkus@gmail.com>
2014-09-04 14:28:08 +0700

    adding contact us view controller

---
+++ CIMB Insurance/CIMB Insurance/SHContactUsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHContactUsViewController.h
+//  CIMB Insurance
+//
+//  Created by Shan Markus on 9/4/14.
+//  Copyright (c) 2014 Vayas World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHContactUsViewController : UIViewController
+
+@end
